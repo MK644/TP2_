906f87570fab7e72c8d8641bb81ff6e806102d9a
hansdiksoker <olivierdorismond1@gmail.com>
2026-04-04 18:37:14 -0400

    Update tp.py

--- tp.py
+++ tp.py
@@ -5,16 +5,16 @@ ll=[[1,2],[3,4],[5,6]]
 print(ll[int(l[1])][int(l[3])])
 
 laby = [[0, 0, 1, 1, 0, 1, 1, 1, 1],
- [0, 0, 0, 1, 0, 1, 1, 1, 1],
- [1, 0, 0, 1, 1, 1, 0, 0, 1],
- [0, 0, 0, 0, 0, 0, 0, 1, 0],
- [1, 0, 1, 0, 0, 0, 0, 0, 1],
- [0, 0, 1, 0, 0, 0, 0, 0, 1],
- [1, 0, 1, 0, 0, 0, 0, 0, 1],
- [0, 0, 0, 0, 0, 1, 0, 0, 1],
- [0, 0, 1, 1, 0, 1, 0, 0, 1],
- [1, 1, 0, 1, 0, 0, 0, 1, 1],
- [1, 1, 1, 0, 1, 1, 0, 0, 0]]
+        [0, 0, 0, 1, 0, 1, 1, 1, 1],
+        [1, 0, 0, 1, 1, 1, 0, 0, 1],
+        [0, 0, 0, 0, 0, 0, 0, 1, 0],
+        [1, 0, 1, 0, 0, 0, 0, 0, 1],
+        [0, 0, 1, 0, 0, 0, 0, 0, 1],
+        [1, 0, 1, 0, 0, 0, 0, 0, 1],
+        [0, 0, 0, 0, 0, 1, 0, 0, 1],
+        [0, 0, 1, 1, 0, 1, 0, 0, 1],
+        [1, 1, 0, 1, 0, 0, 0, 1, 1],
+        [1, 1, 1, 0, 1, 1, 0, 0, 0]]
 
 
 class Labyrinte:
@@ -22,15 +22,39 @@ class Labyrinte:
         self.lab = lab
         self.depart = depart
         self.arrivee = arrivee
+        self.objet=Graphe()
+        lignes = len(self.lab)
+        colonnes = len(self.lab[0])
+# ajout les voisin
+        for x in range(lignes):
+            for y in range(colonnes):
+
+                if self.lab[x][y] == 0:
+                    point = (x, y)
+
+                    directions = [(x - 1, y),  (x + 1, y), (x, y - 1),  (x, y + 1)]
+                    for nx, ny in directions:
+
+                        if 0 <= nx < lignes and 0 <= ny < colonnes:
+                            if self.lab[nx][ny] == 0:
+                                voisin = (nx, ny)
+                                self.objet.ajouteArete(point, voisin)
+                                
+
+
 
 
     #dfs
-    def parcocours_profondeur_laby(self, G,depart,arrivee):
+
+    def parcours_profondeur_laby(self, G,depart,arrivee):
         '''Parcours d'un graphe en profondeur Ã  partir du sommet dÃ©part'''
-        marque = set()  # ensemble des sommets deja visitÃ©s
+
+        marque = set()# ensemble des sommets deja visitÃ©s
         if depart==arrivee:
             print(marque)
+
         def parcours_aux(G, som):
+
             marque.add(som)
 
             for voisin in som.listeVoisins():
@@ -53,5 +77,13 @@ class Labyrinte:
                 for s2 in s.listeVoisins():
                     if s2 not in marque:
                         f.enfile(s2)
+
+    def solutiondfst(self):
+        return self.parcours_profondeur_laby(self.objet,self.depart,self.arrivee)
+
+
+
+c=Labyrinte(laby,'(0,0)', "(4,4)")
+print(c.solutiondfst())
 
 
